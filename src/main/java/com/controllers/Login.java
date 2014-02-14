bad61ae78f139778697d2d1fb538cb2a1ac558ef
maany <imptodefeat@gmail.com>
2014-02-14 18:30:17 +0530

    servlet is working. checking db connection

--- src/main/java/com/controllers/Login.java
+++ src/main/java/com/controllers/Login.java
@@ -40,9 +40,17 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
 		out.write("hello from servlet <br>");
 		
-		
+		try {
+			out.write("trying to connect");
+			Connection con = Config.getRemoteDB();
+			out.write("connection created");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }

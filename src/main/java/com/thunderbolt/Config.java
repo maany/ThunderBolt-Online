f5de6268419ea2efb0ce531f5cf029e5ef1196c0
maany <imptodefeat@gmail.com>
2014-02-14 15:43:17 +0530

    testing for db connections

---
+++ src/main/java/com/thunderbolt/Config.java
@@ -0,0 +1,22 @@
+package com.thunderbolt;
+import java.sql.*;
+public class Config {
+	/**
+	 * This method will return connection of the database resting in openshift
+	 * @throws SQLException 
+	 * @returns Connection con -> connection to the openshift db
+	 */
+	public static Connection getRemoteDB() throws SQLException
+	{
+		Connection con=null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			 con = DriverManager.getConnection("mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/","adminGLgPs7R","wsCktYRn5H8g");
+			 System.out.println("Connection estd with remote database");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>ThunderBolt : Login</title>
+</head>
+<body>
+<center>
+<div class='form'>
+<form method= 'POST' action = 'login'>
+<center>Roll No : <input type = "text" name = "roll"/></center><br>
+<center>Password :  <input type = "password" name="pass"/></center><br><br>
+<center><input type="submit" value = "Login"/>  <a href="reset.html">forgot password</a></center>
+</form>
+</div>
+</center>
+</body>
+</html>

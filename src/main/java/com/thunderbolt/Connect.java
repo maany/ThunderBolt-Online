1a231cade27c323d4f74611a1dff6e02f1cb0836
maany <imptodefeat@gmail.com>
2014-02-18 23:30:32 +0530

    connection changed to deployment mode

--- src/main/java/com/thunderbolt/Connect.java
+++ src/main/java/com/thunderbolt/Connect.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class Connect {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException
 	{
-		Connection con = production();
+		Connection con = deployment();
 		return con;
 	}
 	/**
